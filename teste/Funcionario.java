5be37d326731d9c3b506bf3956cf5bc969711e1e
EmmanuelBMoraes <emmanuelbmoraes@gmail.com>
2023-10-16 09:59:10 -0300

    teste
    teste :sparkles: 2

---
+++ teste/Funcionario.java
@@ -0,0 +1,15 @@
+public class Funcionario {
+	protected String matricula;
+	protected double salario;
+	protected String CPF;
+	protected String nome;
+	protected String setor;
+	
+	public Funcionario(String matricula, double salario, String CPF, String nome, String setor) {
+		this.matricula = matricula;
+		this.salario = salario;
+		this.CPF = CPF;
+		this.nome = nome;
+		this.setor = setor;
+	}
+}
