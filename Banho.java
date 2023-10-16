c68510dc2665672316db7374d4bda7f03f2dfac9
Rafael Marques <rm180249@gmail.com>
2023-10-15 23:58:04 -0300

    :sparkles:Criando Escopo Inicial do Serviço Banho.

---
+++ Banho.java
@@ -0,0 +1,13 @@
+public class Banho extends ModeloServiço {
+
+    private String animal;
+    private String funcionario;
+
+
+    public Banho(Float preço, Integer codigo, Integer dia, Integer mes, Integer ano, Integer hora, Integer minutos, String animal, String funcionario) {
+        super(preço, codigo, dia, mes, ano, hora, minutos);
+        this.animal = animal;
+        this.funcionario = funcionario;
+    }
+    
+}
