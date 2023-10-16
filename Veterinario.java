6f631aee0e91b897330012c9d5246a2197bdc056
EmmanuelBMoraes <emmanuelbmoraes@gmail.com>
2023-10-16 09:27:28 -0300

    feat :sparkles: test first commit

---
+++ Veterinario.java
@@ -0,0 +1,12 @@
+package entidades;
+
+public class Veterinario extends Funcionario{
+	private String CRMV;
+	private String especializacao;
+	
+	public Veterinario(String matricula, double salario, String CPF, String nome, String setor, String CRMV, String especializacao) {
+		super(matricula, salario, CPF, nome, setor);
+		this.CRMV = CRMV;
+		this.especializacao = especializacao;
+	}
+}
