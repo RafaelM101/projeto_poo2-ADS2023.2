ef7874c90a2ae390d512c9331de88bc32603e9ec
RAFAEL MARQUES BEZERRA SOBRAL <20231ADSGR0235@labs.garanhuns.ifpe>
2023-10-23 20:28:59 -0300

    :recycle: Refatorando lógica de serviços.

--- src/funcionarios/funcionarios/Funcionario.java
+++ src/funcionarios/funcionarios/Funcionario.java
@@ -14,4 +14,44 @@ public class Funcionario {
 		this.nome = nome;
 		this.setor = setor;
 	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSetor() {
+		return setor;
+	}
+
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
 }
