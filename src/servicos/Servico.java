49a87639c8b99b1a196aa16b2a06950490b1bb79
Rafael Marques <rm180249@gmail.com>
2023-11-04 14:56:26 -0300

    :sparkles: Incluindo escopo inicial do método de cadastrar um novo serviço.

--- src/servicos/Servico.java
+++ src/servicos/Servico.java
@@ -1,35 +1,36 @@
 package servicos;
 
 import Pets.Pet;
+import components.CRUD;
 import funcionarios.Funcionario;
 
-import java.time.LocalTime;
+import java.util.Scanner;
 
-public class Servico {
+
+public class Servico implements CRUD {
 
     private Funcionario nome_funcionario;
     private Float preco;
     private ListaServicos tipo_agendamento;
     private Pet pet_agendamento;
-    private LocalTime horario_agendamento;
 
-
-
-    public Servico(Funcionario nome_funcionario, Float preco) {
-
+    public Servico(Funcionario nome_funcionario, Float preco, ListaServicos tipo_agendamento, Pet pet_agendamento) {
+        this.nome_funcionario = nome_funcionario;
+        this.preco = preco;
+        this.tipo_agendamento = tipo_agendamento;
+        this.pet_agendamento = pet_agendamento;
     }
 
-    public void agendar() {
-              
+    public static void cadastrar() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Insira a matrícula do Funcionário que irá realizar esse serviço: ");
+        String matricula = input.next();
+        if(Funcionario.consultarFuncionario(matricula)!=null){
+            
+        }
     }
-    
-    public void cancelar() {
-    	
-    }
-    
-    public void efetuar() {
-    	
-    }
-    
+    public static void listar() {};
+    public static void atualizar() {};
+    public static void deletar() {};
 }
 
