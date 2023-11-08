cd9fe52fd34a6a7f83bfa2a9fc59576e42c60750
Rafael Marques <rm180249@gmail.com>
2023-11-08 11:50:48 -0300

    resolvendo conflitos

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,26 +18,7 @@ import servicos.Servico;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-            System.out.println("\nCADASTRANDO TUTOR:");
-            Tutor.cadastrar();
-            System.out.println("\nCADASTRANDO PET:");
-            Pet.cadastrar();
-            System.out.println("\nCADASTRANDO FUNCIONARIO:");
-            Funcionario.cadastrar();
-            System.out.println("\nCADASTRANDO SERVIÇO:");
-            Servico.cadastrar();
-            System.out.println("\nCADASTRANDO SERVIÇO:");
-            Servico.cadastrar();
-            System.out.println("\nIMPRIMINDO AGENDA ATUALIZADA:");
-            Funcionario funcionario = Funcionario.consultarFuncionario("123");
-            funcionario.listarHorarios();
-
-            Servico.cadastrar();
-
-            System.out.println("Pressione Enter para voltar ao menu inicial...");
-            input.nextLine(); // Aguarda a tecla Enter
         }
 
     }
