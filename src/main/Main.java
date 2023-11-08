f6debc8e1aa287bef4783c8c6c56c22eea7954d1
Rafael Marques <rm180249@gmail.com>
2023-11-08 14:11:55 -0300

    :bug: Correção de bugs no Metódo Atualizar da entidade Serviço.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,24 +1,23 @@
 package main;
 
-import java.time.LocalDate;
 
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
-
-import Pets.Gato;
 import Pets.Pet;
 import Tutores.Tutor;
 import funcionarios.Funcionario;
-import servicos.AgendaDia;
 import servicos.Servico;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Tutor.cadastrar();
+        Pet.cadastrar();
+        Funcionario.cadastrar();
+        Servico.cadastrar();
+        Servico.listar();
+        Servico.atualizar();
+        Servico.listar();
+        Servico.deletar();
+        Servico.listar();
         }
 
     }
