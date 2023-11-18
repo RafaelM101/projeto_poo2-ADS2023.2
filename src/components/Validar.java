eca12620e40e63d3025cb38e5be342eb7ef28186
EmmanuelBMoraes <emmanuelbmoraes@gmail.com>
2023-11-18 12:29:56 -0300

    :sparkles: implementação da validação cpf

---
+++ src/components/Validar.java
@@ -0,0 +1,23 @@
+package components;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import exceptions.CpfInvalidoException;
+
+
+public class Validar {
+    //regex valida cpf
+    private static String regexCpf = "^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$";
+    //cria o patter pra usar o regex
+    static Pattern pattern = Pattern.compile(regexCpf);
+    //faz a operação
+    static Matcher matcher;
+    public static boolean validarCPF(String validarCpf) throws CpfInvalidoException{
+        matcher = pattern.matcher(validarCpf);
+        if(matcher.matches()) {
+            System.out.println("CPF VERIFICADO E VALIDADO COM SUCESSO!");
+            return true;
+        }
+        throw new CpfInvalidoException("CPF INVÁLIDO");
+    }
+}
